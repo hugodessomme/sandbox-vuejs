e6be6ed686c7cf275a7b359a921034867530e413
hugodessomme <hdessomme@gmail.com>
2019-04-07 13:38:14 +0200

    Computed, watchers, bug fix

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * 1. Rename this file to config.local.js
+ * 2. Add your configuration in the config object
+ * 3. Use import statement in your JavaScript file to access your config data
+ */
+
+const config = {
+    currency_converter_api_key: "YOUR_API_KEY",
+};
+
+export default config;
